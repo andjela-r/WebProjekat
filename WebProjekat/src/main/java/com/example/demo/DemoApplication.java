df81e7bfcf5609f6ae05639bca6ada799b6aa186
andjela-r <kixiradojevic@gmail.com>
2022-04-12 11:49:18 +0200

    Obrisala sam Repositorijume demo projekta

--- WebProjekat/src/main/java/com/example/demo/DemoApplication.java
+++ WebProjekat/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import com.example.demo.entity.Employee;
-import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
