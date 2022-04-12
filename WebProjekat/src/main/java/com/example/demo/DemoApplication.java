4ca0da49348c6633f744fcbd765069b68ec9c08d
djurick <djuric.kristina01@gmail.com>
2022-04-12 19:21:30 +0200

    Popravila error sa Autowired-om

--- WebProjekat/src/main/java/com/example/demo/DemoApplication.java
+++ WebProjekat/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
 import com.example.demo.entity.Korisnik;
-import com.example.demo.repository.KorisnikRepository;
-
+import com.example.demo.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -17,6 +18,7 @@ import static com.example.demo.entity.Uloga.MENADZER;
    @SpringBootApplication anotacija nastala je od @EnableAutoConfiguration anotacije koja
    upravlja konfiguracijom aplikacije.
  */
+
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 	/* Da bismo testirali repozitorijum, direktno smo pozvali u glavnoj klasi metode,
