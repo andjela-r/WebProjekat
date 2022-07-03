b368ccc1516d2a1e86b9ea6d725910dddef9afec
djurick <djuric.kristina01@gmail.com>
2022-07-03 16:34:08 +0200

    pokusaj update-a i dodavanja artikala

--- WebProjekat/src/main/java/com/example/demo/service/ArtikliService.java
+++ WebProjekat/src/main/java/com/example/demo/service/ArtikliService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Artikli;
+import com.example.demo.entity.Kupac;
 import com.example.demo.repository.ArtikliRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +9,13 @@ import org.springframework.stereotype.Service;
 public class ArtikliService {
 
     private ArtikliRepository artikliRepository;
+
+    public Artikli getById(Long id) {
+
+        return artikliRepository.getOne(id);
+    }
+    public void updateArtikli(Artikli a){
+        artikliRepository.save(a);
+    }
 
 }
